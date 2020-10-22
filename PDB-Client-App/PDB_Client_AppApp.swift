e5c19d5b8cd509a0185c8dd25b4c32a36e17d036
Ben <benjaminjcramer43@gmail.com>
2020-10-22 12:42:04 -0500

    First commit

---
+++ PDB-Client-App/PDB_Client_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDB_Client_AppApp.swift
+//  PDB-Client-App
+//
+//  Created by Ben Cramer on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct PDB_Client_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
